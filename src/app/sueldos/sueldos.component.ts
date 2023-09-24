8cb1931bb1a16af49ae4ed00e04c9e400fafce9c
Jose <jose.rivera.jr6445@gmail.com>
2023-09-23 21:15:20 -0600

    ISSS Multiplicada

--- src/app/sueldos/sueldos.component.ts
+++ src/app/sueldos/sueldos.component.ts
@@ -18,11 +18,15 @@ export class SueldosComponent {
   CalcularS(){  
     const sueldo = this.SueldoI;
     const AFPts = parseFloat(this.AFP); // Convierte a número de punto flotante
+    const ISSES = parseFloat(this.ISSS); // Convierte a número de punto flotante
   // Multiplicar el valor de AFP por 7.25%
   const AFPMultiplicada = AFPts * 0.0725;
+   // Multiplicar el valor de ISSES por 3%
+   const ISSESMultiplicada = ISSES * 0.03;
 
   console.log("Sueldo Inicial:", sueldo);
   console.log("Deducciones AFP (después de multiplicar por 7.25%):", AFPMultiplicada);
+  console.log("Deducciones ISSS (después de multiplicar por 3%):", ISSESMultiplicada);
 
   }
 
