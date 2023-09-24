df4d3429309a6b8ce35679d1bec71e849913fa7c
miguel <magr10007pz@gmail.com>
2023-09-23 21:51:15 -0600

    Calcular sueldo final

--- src/app/sueldos/sueldos.component.ts
+++ src/app/sueldos/sueldos.component.ts
@@ -10,24 +10,51 @@ import { BrowserModule } from '@angular/platform-browser';
 
 export class SueldosComponent {
 
-  SueldoI = '';
-  AFP = '';
-  RENTA = '';
-  ISSS = '';
+  SueldoI = 0;
+  AFP = 0;
+  RENTA = 0;
+  ISSS = 0;
+  SueldoF = 0;
 
-  CalcularS(){  
-    const sueldo = this.SueldoI;
-    const AFPts = parseFloat(this.AFP); // Convierte a número de punto flotante
-    const ISSES = parseFloat(this.ISSS); // Convierte a número de punto flotante
-  // Multiplicar el valor de AFP por 7.25%
-  const AFPMultiplicada = AFPts * 0.0725;
-   // Multiplicar el valor de ISSES por 3%
-   const ISSESMultiplicada = ISSES * 0.03;
+  suedoR = 0;
 
-  console.log("Sueldo Inicial:", sueldo);
-  console.log("Deducciones AFP (después de multiplicar por 7.25%):", AFPMultiplicada);
-  console.log("Deducciones ISSS (después de multiplicar por 3%):", ISSESMultiplicada);
+  
 
+  CalcularAFP(sueldoC: number){
+    this.AFP = sueldoC * 0.0725
+  }
+
+  CalcularISSS(sueldoC: number){
+    this.ISSS = sueldoC * 0.03 
+    if(this.ISSS > 30){
+      this.ISSS = 30
+    } 
+  }
+
+
+  CalcularRenta(sueldoC: number){
+    
+    if(sueldoC < 472){
+      this.RENTA = 0;
+    } else if(sueldoC > 472 && sueldoC < 895.25 ){
+      this.RENTA = ((this.suedoR - 472.00) * 0.10) + 17.67
+    } else if(sueldoC > 895.25 && sueldoC < 2038.10){
+      this.RENTA = ((this.suedoR - 895.24) * 0.20) + 60
+    } else {
+      this.RENTA = ((this.suedoR - 2038.10) * 0.30) + 288.57
+    }
+  }
+
+  CalcualrSL(){
+    this.SueldoF = this.SueldoI - this.RENTA - this.AFP - this.ISSS
+  }
+
+  CalcularS(){
+    this.CalcularAFP(this.SueldoI);
+    this.CalcularISSS(this.SueldoI);
+    this.suedoR = this.SueldoI - this.AFP - this.ISSS
+    this.CalcularRenta(this.suedoR);
+    this.CalcualrSL();
   }
 
 
