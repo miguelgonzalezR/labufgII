63468540d027ddbd33e4a9eb3deca7ac8cc83fec
miguel <magr10007pz@gmail.com>
2023-09-23 20:17:22 -0600

    Se obtiene en sueldo de input

--- src/app/sueldos/sueldos.component.ts
+++ src/app/sueldos/sueldos.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-sueldos',
   templateUrl: './sueldos.component.html',
   styleUrls: ['./sueldos.component.css']
 })
+
 export class SueldosComponent {
 
+  valorInput = '';
+
+  CalcularS(){
+    console.log("hola mundo")
+    alert("hola mundo")
+    const valor = this.valorInput;
+    console.log('Valor del input:', valor);
+  }
+
+
+  constructor(){}
+  ngOnInit(): void{}
 }
